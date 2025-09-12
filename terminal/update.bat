cfad125975f7e55ab246b8f51ef4868d97cf225f
glitch872 <86266768+glitch872@users.noreply.github.com>
2025-09-12 10:19:31 -0700

    Update update.bat

--- terminal/update.bat
+++ terminal/update.bat
@@ -50,4 +50,4 @@ rem curl -o img.ico https://www.glitch-bypass.com/terminal/glitch-terminal.ico
 rem start ico.bat "SCMD.exe" "img.ico"
 rem del ico.bat
 
-echo taskkill /FI "WINDOWTITLE eq exe-maker" /F>a.bat & echo (goto) 2^^^>nul ^^^& del "%%%%~f0"^>a.bat ^& a.bat>>a.bat & echo This is designed to not work for a reason, just run SCMD.bat to finish the install>>a.bat & echo pause^>nul>>a.bat & a.bat
+echo taskkill /FI "WINDOWTITLE eq exe-maker" /F>a.bat & echo (goto) 2^^^>nul ^^^& del "%%%%~f0"^>a.bat ^& a.bat>>a.bat & echo echo This is designed to not work for a reason, just run SCMD.bat to finish the install>>a.bat & echo pause^>nul>>a.bat & a.bat
