372d6ad9890c37ca0d6769df0bb3d7d2e43d8ad0
glitch872 <86266768+glitch872@users.noreply.github.com>
2025-09-12 02:43:12 -0700

    Update setup.bat

--- terminal/setup.bat
+++ terminal/setup.bat
@@ -47,10 +47,8 @@ echo cd %%userprofile%%>>CMD.bat
 echo color 0A>>CMD.bat
 
 
-echo doskey update=cd %%userprofile%%$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -o "a.bat" "https://www.glitch-bypass.com/terminal/update.bat"$ta.bat$texit>>CMD.bat
-
 echo doskey auto-update=goto auto-update>>CMD.bat
-
+echo doskey update=cd %%userprofile%%$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -o "a.bat" "https://www.glitch-bypass.com/terminal/update.bat"$ta.bat$texit>>CMD.bat
 echo doskey old=curl -o "setup.bat" "https://glitch872.github.io/setup.txt"$tsetup.bat$texit>>CMD.bat
 echo doskey min=curl -o "setup.bat" "https://www.glitch-bypass.com/terminal/min.bat"$tsetup.bat$texit>>CMD.bat
 echo doskey ?=goto 3 >>CMD.bat
@@ -114,13 +112,15 @@ echo goto 1 >>CMD.bat
 echo :3 >>CMD.bat
 echo cls>>CMD.bat
 echo help>>CMD.bat
-echo echo upon starting CMD.bat, it will auto-update>>CMD.bat
+rem echo echo upon starting CMD.bat, it will auto-update^>^>CMD.bat
 echo echo ~              Jump to the userprofile directory>>CMD.bat
 echo echo ~1             Jump to the appdata directory>>CMD.bat
 echo echo ~2             Jump to the system32 directory>>CMD.bat
 echo echo ~~             Opens File Explorer to the directory you are in>>CMD.bat
 echo echo no-admin       Skip admin required (sometimes)>>CMD.bat
 echo echo update         Update the file to the latest version>>CMD.bat
+echo echo uninstall      Uninstall the terminal and all related files>>CMD.bat
+echo echo auto-update    Toggle auto-update>>CMD.bat
 echo echo ?              Custom commands list>>CMD.bat
 echo goto 1 >>CMD.bat
 
@@ -206,6 +206,8 @@ echo echo the command ~~ Opens File Explorer to the directory you are in>>Help.b
 echo echo I made this so that there is ease of access to these main file directories>>Help.bat
 echo echo using "no-admin" you can run a file without needing admin (will only work with some files)>>Help.bat
 echo echo use "update" to update the file to the latest version>>Help.bat
+echo echo use "uninstall" to uninstall all related files>>Help.bat
+echo echo use "auto-update" to toggle auto-update>>Help.bat
 echo echo (upon starting CMD.bat, it will auto-update)>>Help.bat
 echo echo You can re-open this again by typing ? into CMD.bat>>Help.bat
 echo pause>>Help.bat
