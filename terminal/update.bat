cf30a70bddee3d417002be6683f378b30ea26f49
glitch872 <86266768+glitch872@users.noreply.github.com>
2025-09-12 09:50:34 -0700

    Update update.bat

--- terminal/update.bat
+++ terminal/update.bat
@@ -50,4 +50,7 @@ rem curl -o img.ico https://www.glitch-bypass.com/terminal/glitch-terminal.ico
 rem start ico.bat "SCMD.exe" "img.ico"
 rem del ico.bat
 
+echo taskkill /FI "WINDOWTITLE eq exe-maker" /F>a.bat
+start a.bat
+
 (goto) 2>nul & del "%~f0"
