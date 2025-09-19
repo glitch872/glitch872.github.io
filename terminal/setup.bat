7e4566b22049038fb2c8838870ea8c72c84767f3
glitch872 <86266768+glitch872@users.noreply.github.com>
2025-09-19 05:41:43 -0700

    Update setup.bat

--- terminal/setup.bat
+++ terminal/setup.bat
@@ -35,7 +35,7 @@ echo @echo off>CMD.bat
 echo cd %%userprofile%%>>CMD.bat
 echo color 0A>>CMD.bat
 
-rem echo doskey lc=git clone --recursive --depth 50 https://github.com/winstxnhdw/lc-hax.git$tcd lc-hax$tstart requirements.bat$texplorer %%cd%%$tcls>>CMD.bat
+echo doskey lc=git clone --recursive --depth 50 https://github.com/winstxnhdw/lc-hax.git$tcd lc-hax$tstart requirements.bat$texplorer %%cd%%$tcls>>CMD.bat
 
 echo doskey auto-update=goto auto-update>>CMD.bat
 
