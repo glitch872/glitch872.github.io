6c8a997218bcfd071f940d04b9cbaca464de91fd
glitch872 <86266768+glitch872@users.noreply.github.com>
2025-09-26 21:14:53 -0700

    Update profileSize.js

--- profileSize.js
+++ profileSize.js
@@ -4,5 +4,8 @@ window.addEventListener('message', (event) => {
     if (iframe && event.data.height) {
       iframe.style.height = event.data.height + 'px';
     }
+    if (iframe && event.data.width) {
+      iframe.style.width = event.data.width + 'px';
+    }
   }
 });
