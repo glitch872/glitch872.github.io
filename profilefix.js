7ace90f15c962ca2f7ffa606f117c258fc79576f
glitch872 <86266768+glitch872@users.noreply.github.com>
2025-09-26 21:13:55 -0700

    Delete profilefix.js

--- profilefix.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener('message', (event) => {
-  if (event.data?.type === 'setHeight') {
-    const iframe = document.getElementByClass('profileIframe');
-    if (iframe && event.data.height) {
-      iframe.style.height = event.data.height + 'px';
-    }
-  }
-});
