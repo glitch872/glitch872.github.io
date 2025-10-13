88acebaaac54281e53649343913a279bc7ebc6d8
glitch872 <86266768+glitch872@users.noreply.github.com>
2025-10-13 02:41:40 -0700

    Create profileSize1.js

---
+++ profileSize1.js
@@ -0,0 +1,11 @@
+window.addEventListener('message', (event) => {
+  if (event.data?.type === 'setHeight') {
+    const iframe = document.getElementById('profileIframe1');
+    if (iframe && event.data.height) {
+      iframe.style.height = event.data.height + 'px';
+    }
+    if (iframe && event.data.width) {
+      iframe.style.width = event.data.width + 'px';
+    }
+  }
+});
