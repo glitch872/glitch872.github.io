1586f7ce08e5954bc2ab4328cdebc3b4e5004a25
glitch872 <86266768+glitch872@users.noreply.github.com>
2026-01-25 22:21:59 -0800

    Refactor setup.bat for path consistency and cleanup
    Updated echo commands to include quotes around userprofile and other paths for consistency and to prevent errors. Removed deprecated curl command and adjusted various echo statements for path handling.

--- terminal/setup.bat
+++ terminal/setup.bat
@@ -13,14 +13,11 @@ REM ------------------------------------------------------------
 
 @echo off
 
-REM Deprecated (historical only — endpoint removed):
-REM curl -L -o "setup.bat" "https://glitch872.github.io/setup.txt" & setup.bat & exit
-
 :: setup
 
 color 0A
 title update/install
-cd %userprofile%
+cd "%userprofile%"
 echo .>update-bat
 cd Desktop
 rd /s /q "Art Project"
@@ -39,7 +36,7 @@ echo @echo off>CMD.bat
 echo. >>CMD.bat
 echo :: Check if first run>>CMD.bat
 echo. >>CMD.bat
-echo cd %%userprofile%%>>CMD.bat
+echo cd "%%userprofile%%">>CMD.bat
 echo IF EXIST first-run-bat (>>CMD.bat
 echo 	goto installed>>CMD.bat
 echo )>>CMD.bat
@@ -63,32 +60,32 @@ echo. >>CMD.bat
 echo :installed >>CMD.bat
 
 
-echo cd %%userprofile%%>>CMD.bat
+echo cd "%%userprofile%%">>CMD.bat
 echo color 0A>>CMD.bat
 
-echo doskey lc=git clone --recursive --depth 50 https://github.com/winstxnhdw/lc-hax.git$tcd lc-hax$tstart requirements.bat$texplorer %%cd%%$tcls>>CMD.bat
+echo doskey lc=git clone --recursive --depth 50 https://github.com/winstxnhdw/lc-hax.git$tcd lc-hax$tstart requirements.bat$texplorer "%%cd%%"$tcls>>CMD.bat
 
 echo doskey auto-update=goto auto-update>>CMD.bat
 
-echo doskey repair=cd %%userprofile%%$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -L -s -o "a.bat" "https://glitch872.github.io/terminal/install.bat"$tstart a.bat$tcls$tcurl -L -s -o "b.bat" "https://glitch872.github.io/terminal/setup.bat"$tb.bat$texit>>CMD.bat
+echo doskey repair=cd "%%userprofile%%"$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -L -s -o "a.bat" "https://glitch872.github.io/terminal/install.bat"$tstart a.bat$tcls$tcurl -L -s -o "b.bat" "https://glitch872.github.io/terminal/setup.bat"$tb.bat$texit>>CMD.bat
 
-echo doskey update-all=cd %%userprofile%%$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -L -s -o "a.bat" "https://glitch872.github.io/terminal/update.bat"$tstart a.bat$tcls>>CMD.bat
-echo doskey update=cd %%userprofile%%$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -L -s -o "a.bat" "https://glitch872.github.io/terminal/setup.bat"$ta.bat$texit>>CMD.bat
+echo doskey update-all=cd "%%userprofile%%"$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -L -s -o "a.bat" "https://glitch872.github.io/terminal/update.bat"$tstart a.bat$tcls>>CMD.bat
+echo doskey update=cd "%%userprofile%%"$tcd Desktop$tcd OneDrive$tcd Desktop$tcurl -L -s -o "a.bat" "https://glitch872.github.io/terminal/setup.bat"$ta.bat$texit>>CMD.bat
 echo doskey trollmode=goto troll>>CMD.bat
 echo doskey min=curl -L -s -o "setup.bat" "https://glitch872.github.io/terminal/min.bat"$tsetup.bat$texit>>CMD.bat
 echo doskey ?=goto 3 >>CMD.bat
-echo doskey ~=cd %%userprofile%%$tcls>>CMD.bat
-echo doskey ~1=cd %%appdata%%$tcls>>CMD.bat
+echo doskey ~=cd "%%userprofile%%"$tcls>>CMD.bat
+echo doskey ~1=cd "%%appdata%%"$tcls>>CMD.bat
 echo doskey >>CMD.bat
 echo cd ..>>CMD.bat
 echo cd ..>>CMD.bat
 echo cd Windows>>CMD.bat
 echo cd System32>>CMD.bat
 echo FOR /F "delims=" %%%%F IN ('cd') DO SET System32=%%%%F>>CMD.bat
-echo doskey ~2=cd %%System32%%$tcls>>CMD.bat
+echo doskey ~2=cd "%%System32%%"$tcls>>CMD.bat
 echo doskey ~~=goto 2 >>CMD.bat
 echo doskey Boot=goto Boot >>CMD.bat
-echo doskey uninstall=cd %%userprofile%%$tdel first-run-bat$tdel no-update$tcd Desktop$trd /s /q "Art Project"$tcd OneDrive$tcd Desktop$tdel *SCMD.exe$texit>>CMD.bat
+echo doskey uninstall=cd "%%userprofile%%"$tdel first-run-bat$tdel no-update$tcd Desktop$trd /s /q "Art Project"$tcd OneDrive$tcd Desktop$tdel *SCMD.exe$texit>>CMD.bat
 echo doskey user-level=goto no-admin >>CMD.bat
 echo doskey find=goto find>>CMD.bat
 echo doskey ls=dir /w>>CMD.bat
@@ -97,7 +94,7 @@ echo doskey clear=cls>>CMD.bat
 echo doskey Discord=goto Discord>>CMD.bat
 echo doskey hide=goto hide >>CMD.bat
 echo doskey exe=curl -L -s -o "bat2exe.bat" "https://glitch872.github.io/terminal/yep.bat"$tgoto 2 >>CMD.bat
-echo cd %%userprofile%%>>CMD.bat
+echo cd "%%userprofile%%">>CMD.bat
 
 echo IF EXIST no-update (>>CMD.bat
 echo     goto skip>>CMD.bat
@@ -146,14 +143,13 @@ echo cls>>CMD.bat
 echo goto 1 >>CMD.bat
 
 echo :2>>CMD.bat
-echo explorer %%cd%%>>CMD.bat
+echo explorer "%%cd%%">>CMD.bat
 echo cls>>CMD.bat
 echo goto 1 >>CMD.bat
 
 echo :3 >>CMD.bat
 echo cls>>CMD.bat
 echo help>>CMD.bat
-rem echo echo upon starting CMD.bat, it will auto-update^>^>CMD.bat
 echo echo ~              Jump to the userprofile directory>>CMD.bat
 echo echo ~1             Jump to the appdata directory>>CMD.bat
 echo echo ~2             Jump to the system32 directory>>CMD.bat
@@ -174,13 +170,12 @@ echo )>>CMD.bat
 echo echo .^>no-update>>CMD.bat
 echo echo auto-update disabled>>CMD.bat
 echo :update-toggle>>CMD.bat
-rem echo pause^>^>CMD.bat
 echo goto 1 >>CMD.bat
 
 echo :Boot>>CMD.bat
-echo cd %%userprofile%%>>CMD.bat
+echo cd "%%userprofile%%">>CMD.bat
 echo cd Desktop>>CMD.bat
-echo cd Art Project\.bat>>CMD.bat
+echo cd "Art Project\.bat">>CMD.bat
 echo call CMD.bat>>CMD.bat
 echo goto 1 >>CMD.bat
 
@@ -208,13 +203,13 @@ echo ping %%a%% -n 1 >>CMD.bat
 echo goto spam-2>>CMD.bat
 
 echo :Discord>>CMD.bat
-echo set save=%%cd%%>>CMD.bat
-echo cd %%userprofile%%>>CMD.bat
+echo set save="%%cd%%">>CMD.bat
+echo cd "%%userprofile%%">>CMD.bat
 echo cd ..>>CMD.bat
 echo cd ..>>CMD.bat
 echo FOR /F "delims=" %%%%F IN ('dir /S /b discord.exe') DO SET ExePath=%%%%F>>CMD.bat
 echo cmd /min /C "set __COMPAT_LAYER=RUNASINVOKER && start "" "%%ExePath%%"">>CMD.bat
-echo cd %%save%%>>CMD.bat
+echo cd "%%save%%">>CMD.bat
 echo goto 1 >>CMD.bat
 
 echo :hide>>CMD.bat
@@ -226,21 +221,21 @@ echo goto 1 >>CMD.bat
 
 echo @echo off>log.bat
 echo title File compiler list>>log.bat
-echo cd %%userprofile%%>>log.bat
+echo cd "%%userprofile%%">>log.bat
 echo mkdir dir>>log.bat
 echo cd ..>>log.bat
 echo cd ..>>log.bat
 echo cls>>log.bat
 echo echo Compiling>>log.bat
-echo dir /s %%cd%%*^>%%userprofile\dir\info.txt>>log.bat
-echo explorer %%userprofile%%\dir\>>log.bat
+echo dir /s "%%cd%%*"^>"%%userprofile%%\dir\info.txt">>log.bat
+echo explorer "%%userprofile%%\dir\">>log.bat
 echo cls>>log.bat
 echo echo done>>log.bat
 echo pause>>log.bat
 
 :: Check if first run
 
-cd %userprofile%
+cd "%userprofile%"
 IF EXIST first-run-bat (
     goto skip
 )
@@ -269,7 +264,6 @@ pause
 cd Desktop
 cd "Art Project"
 cd .bat
-rem explorer %cd%
 start CMD
 del Help.bat
 
