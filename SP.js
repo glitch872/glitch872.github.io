be0fd4e6ec26f44dd78c718b29e5f752148f26a9
glitch872 <86266768+glitch872@users.noreply.github.com>
2026-08-02 14:57:28 -0700

    Add SP.js for temporary invite unavailability page

---
+++ SP.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Invite Temporarily Unavailable</title>
+</head>
+<body>
+    <h1>Sorry!</h1>
+    <p>Our invite URL has been yoinked.</p>
+    <p>Wait for a bit while we get a new one up and running!</p>
+</body>
+</html>
